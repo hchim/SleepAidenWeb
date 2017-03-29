dae73fe73ff9ef43793bbae2042a898542e73b00
hchen <hchen229@gmail.com>
2017-03-29 08:39:37 -0700

    Add tos and pp pages

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'SleepAiden' });
 });
 
+router.get('/tos', function (req, res, next) {
+  res.render('terms-of-service')
+})
+
+router.get('/pp', function (req, res, next) {
+  res.render('privacy-policy')
+})
+
 module.exports = router;
